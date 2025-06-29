4365e00f5c1f87512fd487585d03fa7c548f2e67
Ahmad Hideo Alfarisi <hideoahmad@gmail.com>
2025-06-29 20:07:21 +0700

    Update: Koneksi DB pakai ENV dari Railway

--- config/database.go
+++ config/database.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -10,15 +12,23 @@ import (
 var DB *sql.DB
 
 func Connect() {
+    dbUser := os.Getenv("MYSQLUSER")
+    dbPass := os.Getenv("MYSQLPASSWORD")
+    dbHost := os.Getenv("MYSQLHOST")
+    dbPort := os.Getenv("MYSQLPORT")
+    dbName := os.Getenv("MYSQLDATABASE")
+
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+
     var err error
-    DB, err = sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/db_barang")
+    DB, err = sql.Open("mysql", dsn)
     if err != nil {
-        log.Fatal("Gagal koneksi DB:", err)
+        log.Fatal("Gagal koneksi database:", err)
     }
 
     if err = DB.Ping(); err != nil {
-        log.Fatal("DB tidak bisa diakses:", err)
+        log.Fatal("Tidak bisa ping database:", err)
     }
 
-    log.Println("Koneksi DB sukses!")
+    log.Println("Koneksi ke database Railway berhasil!")
 }
